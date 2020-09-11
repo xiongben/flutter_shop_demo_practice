5af98eab963f9b309cb7dd8dc8a615df068a5a09
xiongben <xiongben1993@gmail.com>
2020-09-11 21:39:11 +0800

    [add] add home page

---
+++ lib/pages/category_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class CategoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("category page"),),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("home page"),),
+    );
+  }
+}
