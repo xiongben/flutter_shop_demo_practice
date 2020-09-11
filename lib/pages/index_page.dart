7d3335f75f2b365ba3f6d73c8955c86328e36ec4
xiongben <xiongben1993@gmail.com>
2020-09-11 20:26:03 +0800

    init

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("human life+"),),
+      body: Center(child: Text("66666"),),
+    );
+  }
+}
